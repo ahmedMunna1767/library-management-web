25159eeaee5c942f29db7d8156aeef13dbf5553b
Ahmed-Munna <munnaa329@gmail.com>
2023-08-15 14:45:56 +0600

    feat: Auth and User modules

--- src/main/java/com/example/librarymanagementweb/LibraryManagementWebApplication.java
+++ src/main/java/com/example/librarymanagementweb/LibraryManagementWebApplication.java
@@ -1,13 +1,45 @@
 package com.example.librarymanagementweb;
 
+import static com.example.librarymanagementweb.user.Role.ADMIN;
+import static com.example.librarymanagementweb.user.Role.MANAGER;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.librarymanagementweb.auth.AuthenticationService;
+import com.example.librarymanagementweb.auth.RegisterRequest;
 
 @SpringBootApplication
 public class LibraryManagementWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryManagementWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementWebApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            AuthenticationService service) {
+        return args -> {
+            var admin = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("admin@mail.com")
+                    .password("password")
+                    .role(ADMIN)
+                    .build();
+            System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+            var manager = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("manager@mail.com")
+                    .password("password")
+                    .role(MANAGER)
+                    .build();
+            System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+        };
+    }
 }
